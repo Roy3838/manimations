f14426748703c094e302e63a6f82b8dd8a6b6280
Roy3838 <a01283783@itesm.com>
2022-08-24 08:14:18 -0500

    eco.py is complete

--- py/eco.py
+++ py/eco.py
@@ -2,6 +2,7 @@ from itertools import count
 from manim import *
 import numpy as np
 from random import randint, uniform
+from ticktock import tick
 
 
 
@@ -11,26 +12,36 @@ class eco(Scene):
 
     def construct(self):
 
-        
-
-
-
-
-        population = np.zeros(10000)
+        self.camera.background_color = "#E2E2E2"
+        clock = tick()
+        population = np.zeros(100000)
         population.fill(10)
+        iterations=40000000
 
-        for i in range(10000000):
-            yi=randint(0,10000-1)
-            yb=randint(0,10000-1)
-            mi=uniform(0,2)
-            if population[yi]>0:
+        
+        for i in range(iterations):
+            yi=randint(0,len(population)-1)
+            yb=randint(0,len(population)-1)
+            mi=uniform(0,1)
+            if population[yb]>0:
                 population[yi]+=mi
                 population[yb]-=mi
+
+            
             c_d = 2
-            if i%1000000==0:
+            if ((i%(iterations/500)==0 and i<=iterations/10) 
+            or (i%(iterations/200)==0 and i>iterations/10)):
+                
+                if (i<iterations/10):
+                    rango=[0, 100000, 10000]
+                elif(i==iterations/10):
+                    rango=[0, 15000, 5000]
+                    ogchart=chart.copy()
+                else:
+                    rango=[0, 15000, 5000]
+                #BARCHART 
                 chart = BarChart(
                     #social class count of people, 20 classes
-                    
                     values=[((0 < population) & (population < c_d)).sum(),
                             ((c_d <= population) & (population < 2*c_d)).sum(),
                             ((2*c_d <= population) & (population < 3*c_d)).sum(),
@@ -50,17 +61,33 @@ class eco(Scene):
                             ((16*c_d <= population) & (population < 17*c_d)).sum(),
                             ((17*c_d <= population) & (population < 18*c_d)).sum(),
                             ((18*c_d <= population) & (population < 19*c_d)).sum(),
-                            ((19*c_d <= population) & (population < 20*c_d)).sum()
+                            ((19*c_d <= population) & (population < 20*c_d)).sum(),
+                            ((20*c_d <= population) & (population < 21*c_d)).sum(),
+                            ((21*c_d <= population) & (population < 22*c_d)).sum(),
+                            ((22*c_d <= population) & (population < 23*c_d)).sum(),
+                            ((23*c_d <= population) & (population < 24*c_d)).sum(),
+                            ((24*c_d <= population) & (population < 25*c_d)).sum(),
                             ],                        
-                y_range=[0, 10000, 1000],
+                y_range=rango,
                 y_length=6,
                 x_length=10,
-                x_axis_config={"font_size": 36})
-                print(i)
+                x_axis_config={"font_size": 36, "color": BLACK},
+                y_axis_config={"font_size": 36, "color": BLACK})
                 
-                self.add(chart)
-                self.wait(0.5)
+                #BARCHART
+
+                print(i/iterations)
+                if i == iterations/10:
+                    self.add(ogchart)
+                    self.play(ReplacementTransform(ogchart, chart.set_color(BLACK)))
+                    self.wait()
+                self.add(chart.set_color(BLACK))
+                self.wait(0.05)
+                self.remove(chart)
+                
+        self.add(chart)
         self.wait()
+        clock.tock()
             
             
             
