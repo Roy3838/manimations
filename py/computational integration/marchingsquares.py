e5e716ef502f20db54b48ac8a8c2ed824353d656
Roy3838 <a01283783@itesm.com>
2022-09-04 22:57:52 -0500

    added Reductibles reference scene

--- py/computational integration/marchingsquares.py
+++ py/computational integration/marchingsquares.py
@@ -16,27 +16,28 @@ class MarchingSquares(ThreeDScene):
             zs=(z_range[1]-z_range[0])/resolution
             
             for i in np.arange(x_range[0],x_range[1],xs):
+                print(i)
                 for j in np.arange(y_range[0],y_range[1],ys):
                     for k in np.arange(z_range[0],z_range[1],zs):
                         #points of the square
-                        #scan=Cube(side_length=xs,fill_opacity=0.8,fill_color=YELLOW).move_to([i,j,k])
-                        #self.play(Write(scan),run_time=0.01)
-                        #self.wait(0.01)
+                        scan=Cube(side_length=xs,fill_opacity=0.8,fill_color=YELLOW).move_to([i,j,k])
+                        self.play(Create(scan),run_time=0.07)
+                        self.wait(0.08)
                         if function(i,j,k)*function(i+xs,j+ys,k+zs) < 0:
-                            cubo=Cube(side_length=xs,fill_opacity=0.5,fill_color=BLUE)
+                            cubo=Cube(side_length=xs,fill_opacity=0.5,fill_color=BLUE_C)
                             cubo.shift(i*RIGHT+j*UP+k*OUT)
                             self.play(Write(cubo),run_time=0.01)
                             #fig = VGroup(*it.chain(*cubo))
-                        #self.remove(scan)
+                        self.remove(scan)
                             
                             
 
 
         
-
+        self.camera.background_color = "#E2E2E2"
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.add(ThreeDAxes())
-        marching_square_algorithm(lambda x,y,z: x**2+y**2+z**2 - 5,[-2.5,2.5],[-2.5,2.5],[-2.5,2.5],30)
+        self.add(ThreeDAxes().set_color(BLACK))
+        marching_square_algorithm(lambda x,y,z: x**2+y**2+z**2 - 5,[-2.5,2.5],[-2.5,2.5],[-2.5,2.5],6)
         #self.play(Write(fig))
         self.wait(2)
         self.begin_ambient_camera_rotation(rate=0.1)
