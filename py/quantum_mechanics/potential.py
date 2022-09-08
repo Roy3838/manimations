dc8f7d2f961b54bdcb571d2a4ab2d42810dc4a70
Roy3838 <a01283783@itesm.com>
2022-09-07 23:53:01 -0500

    potential update

--- py/quantum_mechanics/potential.py
+++ py/quantum_mechanics/potential.py
@@ -6,77 +6,86 @@ import numpy as np
 class Potential(Scene):
     def construct(self):
 
-        def U(x):
+        def pot(x):
             return np.arctan(x)*(np.exp(-x**2)) + 0.4
 
-        def F(x):
+        def force(x):
             gradiente= (  np.exp(-x**2) * (1- 2 * (x**3 + x)* np.arctan(x) ) )/(x**2 + 1)
             val = gradiente*(-9.81)
             return val
 
-        def dx(x):
+        def derivative(x):
             gradiente= (  np.exp(-x**2) * (1- 2 * (x**3 + x)* np.arctan(x) ) )/(x**2 + 1)
             return gradiente
 
 
         
-        def animate_ball(x_0,v_0):
+        def animate_ball(x_0,v_0,
+                        particulas, 
+                        U_x, F,
+                        dx=0.005, 
+                        t_max=3, 
+                        size=0.1,
+                        color=RED,
+                        rnd_v=True,
+                        rnd_x=True):
             E=0.7
 
             #v = lambda x :np.sqrt((E-U(x))*2*m) + v_0
 
-
-
-            ball = Dot(size=size).move_to([x_0,U(x_0),0])
+            # Reference Ball
+            ball = Dot().move_to([x_0,U_x(x_0),0])
             ball.set_color(color)
+            
 
-            E_lim=Line(axes.c2p(0.3537,0.7) + 8*LEFT,axes.c2p(0.3537,0.7)+ 3*RIGHT).set_color(GREEN)
-            E_text=MathTex(r"E_{total}").next_to(E_lim,RIGHT).set_color(GREEN)
-
-            self.add(E_lim,E_text)
-
-            dx=0.005
-            #dx=0.05
-            #t = np.arange(0, 3, dx)
-            particulas=40
-            v = [np.arange(0, 3, dx) for i in range(particulas)]
-            x = [np.arange(0, 3, dx) for i in range(particulas)]
 
             
-            print(len(x))
-            print(len(x[0]))
-            #euler integration
-            #make v_0
-            for vs in range (particulas):
-                v[vs][0] = v_0 + np.random.normal(0,0.4)
 
-            #make x_0
-            for xs in range(particulas):
-                x[xs][0] = x_0 #+ np.random.normal(0,0.6)
+            # Make velocity and position vectors
+            v = [np.arange(0, t_max, dx) for i in range(particulas)]
+            x = [np.arange(0, t_max, dx) for i in range(particulas)]
+
+            # Randomize v_0 or not
+            if rnd_v:
+                for vs in range (particulas):
+                    v[vs][0] = v_0 + np.random.normal(0,0.5)
+            else:
+                for vs in range (particulas):
+                    v[vs][0] = v_0
+
+            # Randomize x_0 or not
+            if rnd_x:
+                for xs in range(particulas):
+                    x[xs][0] = x_0 + np.random.normal(0,0.05)
+            else:
+                for xs in range(particulas):
+                    x[xs][0] = x_0
             
+            # Compute the position and velocity of the particles every t as j
             for j in range(len(x[0])-1):
                 balls=VGroup()
                 balls1d=VGroup()
                 for i in range(len(x)):
                     v[i][j+1] = v[i][j] + F(x[i][j])*dx
                     x[i][j+1] = x[i][j] + v[i][j]*dx
-                    if U(x[i][j+1]) > 0.7:
-                        #shift balls so they are over the graph and not on the graph
-                        #derivative_value=dx(x[i][j+1])
-
-
-                        balls.add(Dot(fill_opacity=0.4).move_to(axes.c2p(x[i][j+1],U(x[i][j+1])) + UP*0.12).set_color(RED))
+                    if U_x(x[i][j+1]) > 0.7:
+                        balls.add(Dot(fill_opacity=0.2).move_to(axes.c2p(x[i][j+1],U_x(x[i][j+1])) + UP*0.12).set_color(color))
                         
                     else:
-                        balls.add(ball.copy().move_to(axes.c2p(x[i][j+1],U(x[i][j+1])) + UP*0.12))
-                        balls1d.add(Dot().move_to(axes.c2p(x[i][j+1],1)).set_color(RED))
+                        balls.add(ball.copy().move_to(axes.c2p(x[i][j+1],U_x(x[i][j+1])) + UP*0.12))
+                        balls1d.add(Dot().move_to(axes.c2p(x[i][j+1],1.25)).set_color(color))
                 self.add(balls,balls1d)
-                print(i)
                 self.wait(1/60)
                 self.remove(balls,balls1d)
 
             
         axes=Axes(x_range=[-3, 2], y_range=[-0.25, 1]).set_color(BLACK)
+        axes.shift(DOWN*1.5)
+
+        oneDaxes=NumberLine(x_range=[-6, 6]).set_color(BLACK)
+        oneDaxes.move_to(axes.c2p(-0.5,1.25))
+        
+        E_max=0.3537
 
 
 
@@ -84,12 +93,21 @@ class Potential(Scene):
         #initial_point = [axes.coords_to_point(-2, U(-2))]
         #dot = Dot(point=initial_point).set_color(BLACK)
         #dot.add_updater(lambda x: x.move_to(axes.c2p( )))
-        graph=axes.plot(lambda x:U(x), x_range=[-3.5,2.5]).set_color(BLACK)
+        graph=axes.plot(lambda x:pot(x), x_range=[-3.5,2.5]).set_color(BLACK)
         label=axes.get_axis_labels(x_label="x", y_label="U(x)").set_color(BLACK)
         #derivative=axes.plot(v).set_color(BLUE)
-        self.play(Create(graph),Create(axes),Create(label))#,Create(derivative))
+        self.play(Create(axes),Create(label),Create(oneDaxes),run_time=1.5)
+        self.play(Create(graph))#,Create(derivative))
+
+        # Energy Label and Line
+        E_lim=Line(axes.c2p(E_max,pot(E_max)) + 8*LEFT,axes.c2p(E_max,pot(E_max))+ 3*RIGHT).set_color(GREEN)
+        E_text=MathTex(r"E_{total}").next_to(E_lim,RIGHT).set_color(GREEN)
+        self.play(Write(E_lim),Write(E_text))
         #self.play(t.animate.set_value(1.25))
-        animate_ball(-2,0)
+        animate_ball(-2,2.3, 1, U_x=pot, F=force, rnd_x=False, rnd_v=False, dx=0.005, color=BLUE)
         self.wait()
+        animate_ball(-2,2.3, 80, U_x=pot, F=force, dx=0.005)
+        self.wait()
+        self.play(Uncreate(graph),Uncreate(axes),Uncreate(label),Uncreate(oneDaxes))
 
 
