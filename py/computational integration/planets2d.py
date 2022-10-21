56b0ceb09a48c6568a37d49c5dbf00e785b6eabd
Roy3838 <a01283783@itesm.mx>
2022-10-21 11:51:25 -0500

    commit

--- py/computational integration/planets2d.py
+++ py/computational integration/planets2d.py
@@ -121,46 +121,110 @@ class Euler(MovingCameraScene):
                 dR1[t+1][:] = dR1i[t+1][:] + 0.5*dt*F1[t+1][:]/m1
                 dR2[t+1][:] = dR2i[t+1][:] + 0.5*dt*F2[t+1][:]/m2
                 dR3[t+1][:] = dR3i[t+1][:] + 0.5*dt*F3[t+1][:]/m3
+        def animacion_frames():
+            # Graficar
+            planet1=Dot(radius=0.8,fill_opacity=1,stroke_width=0).set_color(YELLOW)
+            planet2=Dot(radius=0.3,fill_opacity=1,stroke_width=0).set_color(GREEN)
+            planet3=Dot(radius=0.15,fill_opacity=1,stroke_width=0).set_color(GREY_C)
 
-        # Graficar
-        planet1=Dot(radius=0.8,fill_opacity=1,stroke_width=0).set_color(YELLOW)
-        planet2=Dot(radius=0.3,fill_opacity=1,stroke_width=0).set_color(GREEN)
-        planet3=Dot(radius=0.15,fill_opacity=1,stroke_width=0).set_color(GREY_C)
+            scalingfactor=(8e+11)/4 #distance scaling factor for scene
+            scalingfactorv=13720/1.5 #velocity vector scaling factor for scene
+            scalingfactorf=np.amax(F1)/1.7 #force vector scaling factor for scene
+            scalingfactorf3=np.amax(F3) #the third object varies a lot in force so it is not used
 
-        scalingfactor=(8e+11)/4 #distance scaling factor for scene
-        scalingfactorv=13720/1.5 #velocity vector scaling factor for scene
-        scalingfactorf=np.amax(F1)/1.7 #force vector scaling factor for scene
-        scalingfactorf3=np.amax(F3) #the third object varies a lot in force so it is not used
+            for i in range(0,n,2):
+                print(str(round((i*100/n),2)) + "%")
+                pos1=np.array([R1[i][0]/scalingfactor,R1[i][1]/scalingfactor,0])
+                pos2=np.array([R2[i][0]/scalingfactor,R2[i][1]/scalingfactor,0])
+                pos3=np.array([R3[i][0]/scalingfactor,R3[i][1]/scalingfactor,0])
 
-        for i in range(0,n,2):
-            print(str(round((i*100/n),2)) + "%")
-            pos1=np.array([R1[i][0]/scalingfactor,R1[i][1]/scalingfactor,0])
-            pos2=np.array([R2[i][0]/scalingfactor,R2[i][1]/scalingfactor,0])
-            pos3=np.array([R3[i][0]/scalingfactor,R3[i][1]/scalingfactor,0])
+                planet1.move_to(pos1)
+                planet2.move_to(pos2)
+                planet3.move_to(pos3)
+                #print(dR3[i][:]/scalingfactorv)
+                v2=np.array([dR2[i][0]/scalingfactorv,dR2[i][1]/scalingfactorv,0])
+                v3=np.array([dR3[i][0]/scalingfactorv,dR3[i][1]/scalingfactorv,0])
+                #print(pos2+v2)
+                Fm1=np.array([F1[i][0],F1[i][1],0])*1.5/np.linalg.norm(np.array([F1[i][0],F1[i][1],0]))
+                Fm2=np.array([F2[i][0],F2[i][1],0])*1.5/np.linalg.norm(np.array([F2[i][0],F2[i][1],0]))
+                Fm3=np.array([F3[i][0],F3[i][1],0])*1.5/np.linalg.norm(np.array([F3[i][0],F3[i][1],0]))
+                #print(Fm3)
 
-            planet1.move_to(pos1)
-            planet2.move_to(pos2)
-            planet3.move_to(pos3)
-            #print(dR3[i][:]/scalingfactorv)
-            v2=np.array([dR2[i][0]/scalingfactorv,dR2[i][1]/scalingfactorv,0])
-            v3=np.array([dR3[i][0]/scalingfactorv,dR3[i][1]/scalingfactorv,0])
-            #print(pos2+v2)
-            Fm1=np.array([F1[i][0],F1[i][1],0])*1.5/np.linalg.norm(np.array([F1[i][0],F1[i][1],0]))
-            Fm2=np.array([F2[i][0],F2[i][1],0])*1.5/np.linalg.norm(np.array([F2[i][0],F2[i][1],0]))
-            Fm3=np.array([F3[i][0],F3[i][1],0])*1.5/np.linalg.norm(np.array([F3[i][0],F3[i][1],0]))
-            #print(Fm3)
+                arrowplanet2=Arrow(pos2, pos2+v2, buff=0,color=GREY_C)
+                arrowplanet3=Arrow(pos3, pos3+v3, buff=0,color=GREY_C)
 
-            arrowplanet2=Arrow(pos2, pos2+v2, buff=0,color=GREY_C)
-            arrowplanet3=Arrow(pos3, pos3+v3, buff=0,color=GREY_C)
-
-            Fuerza1=Arrow(pos1,pos1+Fm1, buff=0,color=GOLD)
-            Fuerza2=Arrow(pos2,pos2+Fm2, buff=0,color=GOLD)
-            Fuerza3=Arrow(pos3,pos3+Fm3, buff=0,color=GOLD)
-            self.add(arrowplanet2,arrowplanet3,Fuerza1,Fuerza2,Fuerza3)
-            self.add(planet1,planet2,planet3)
-            self.wait(1/60)
-            self.remove(planet1,planet2,planet3,arrowplanet2,arrowplanet3,Fuerza1,Fuerza2,Fuerza3)
+                Fuerza1=Arrow(pos1,pos1+Fm1, buff=0,color=GOLD)
+                Fuerza2=Arrow(pos2,pos2+Fm2, buff=0,color=GOLD)
+                Fuerza3=Arrow(pos3,pos3+Fm3, buff=0,color=GOLD)
+                self.add(arrowplanet2,arrowplanet3,Fuerza1,Fuerza2,Fuerza3)
+                self.add(planet1,planet2,planet3)
+                self.wait(1/60)
+                self.remove(planet1,planet2,planet3,arrowplanet2,arrowplanet3,Fuerza1,Fuerza2,Fuerza3)
+            
+            #self.add(Text("hi"))
+            print(str(time.time()-start) + " seconds")
         
-        #self.add(Text("hi"))
-        print(str(time.time()-start) + " seconds")
+        def animacion_vectores():
+            planet1=Dot(radius=0.8,fill_opacity=1,stroke_width=0).set_color(YELLOW)
+            planet2=Dot(radius=0.3,fill_opacity=1,stroke_width=0).set_color(GREEN)
+            planet3=Dot(radius=0.15,fill_opacity=1,stroke_width=0).set_color(GREY_C)
+            for i in range(0,n,20):
+                scalingfactor=(8e+11)/4 #distance scaling factor for scene
+                scalingfactorv=13720/1.5 #velocity vector scaling factor for scene
+                scalingfactorf=np.amax(F1)/1.7 #force vector scaling factor for scene
+                scalingfactorf3=np.amax(F3) #the third object varies a lot in force so it is not used
+
+
+
+                pos1=np.array([R1[i][0]/scalingfactor,R1[i][1]/scalingfactor,0])
+                pos2=np.array([R2[i][0]/scalingfactor,R2[i][1]/scalingfactor,0])
+                pos3=np.array([R3[i][0]/scalingfactor,R3[i][1]/scalingfactor,0])
+                v2=np.array([dR2[i][0]/scalingfactorv,dR2[i][1]/scalingfactorv,0])
+                v3=np.array([dR3[i][0]/scalingfactorv,dR3[i][1]/scalingfactorv,0])
+                Fm1=np.array([F1[i][0],F1[i][1],0])*1.5/np.linalg.norm(np.array([F1[i][0],F1[i][1],0]))
+                Fm2=np.array([F2[i][0],F2[i][1],0])*1.5/np.linalg.norm(np.array([F2[i][0],F2[i][1],0]))
+                Fm3=np.array([F3[i][0],F3[i][1],0])*1.5/np.linalg.norm(np.array([F3[i][0],F3[i][1],0]))
+
+
+                planet1.move_to(pos1)
+                planet2.move_to(pos2)
+                planet3.move_to(pos3)
+                arrowplanet2=Arrow(pos2, pos2+v2, buff=0,color=GOLD)
+                arrowplanet3=Arrow(pos3, pos3+v3, buff=0,color=GOLD)
+
+                Fuerza1=Arrow(pos1,pos1+Fm1, buff=0,color=RED)
+                Fuerza2=Arrow(pos2,pos2+Fm2, buff=0,color=RED)
+                Fuerza3=Arrow(pos3,pos3+Fm3, buff=0,color=RED)
+
+                planetpos1=Arrow(ORIGIN,pos1,color=BLACK)
+                planetpos3=Arrow(ORIGIN,pos3,color=BLACK)
+                planetpos2=Arrow(ORIGIN,pos2,color=BLACK)
+
+                
+
+                if i==200 or i ==220:
+                    
+                    self.wait()
+                    self.play(Create(planet1),Create(planet2),Create(planet3))
+                    self.play(Create(planetpos1),Create(planetpos2),Create(planetpos3))
+                    self.wait()
+                    self.play(Create(Fuerza2),Create(Fuerza3))
+                    self.wait()
+                    self.play(Create(arrowplanet2),Create(arrowplanet3))
+                    self.wait()
+
+                    self.play(Uncreate(Fuerza1),Uncreate(Fuerza2),Uncreate(Fuerza3),
+                    Uncreate(planetpos1),Uncreate(planetpos2),Uncreate(planetpos3),
+                    Uncreate(arrowplanet2),Uncreate(arrowplanet3))
+
+
+                    
+
+
+                
+        
+        
+        #animacion_frames()
+        animacion_vectores()
+
         
