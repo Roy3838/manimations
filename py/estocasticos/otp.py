e76aba7e66c2a9ea9e68125a78c1de836a23e85b
Roy3838 <a01283783@itesm.com>
2022-11-06 20:43:20 -0600

    otep update

--- py/estocasticos/otp.py
+++ py/estocasticos/otp.py
@@ -53,6 +53,31 @@ class OTP(Scene):
         # set background color
 
         self.camera.background_color = "#E2E2E2"
+        def intro():
+            # Funcion encripcion
+            message = MathTex("Mensaje").set_color(BLACK).shift(2*UP)
+            encryption_f_x= MathTex("Encripcion(","Mensaje",", ","Llave",") = ", "Cypher").set_color(BLACK)
+            decryption_f_x= MathTex("Decripcion(","Cypher",", ","Llave",") = ","Mensaje").set_color(BLACK)
+
+            # encryption and decryption equations
+            enc_eq = MathTex("Cypher = Texto \\oplus Llave").scale(1.5)
+            dec_eq = MathTex("Texto = Cypher \\oplus Llave").scale(1.5)
+
+            # box en la llave
+            key_box = SurroundingRectangle(encryption_f_x[3], buff = 0.1).set_color(GOLD)
+
+            self.play(Write(message))
+            self.play(
+                message.animate.move_to(encryption_f_x[1].get_center()),
+                Write(encryption_f_x[0]),
+                Write(encryption_f_x[2:])
+                )
+            self.play(Write(key_box))
+            self.wait(1)
+            self.play(FadeOut(encryption_f_x), FadeOut(message), FadeOut(key_box))
+
+
+
         def xor_is_reversible_animation():
 
             # xor operation equations LaTeX
@@ -63,14 +88,54 @@ class OTP(Scene):
             bits3=MathTex("1 \\oplus 0 = 1").scale(0.7).next_to(bits2,DOWN).set_color(BLACK)
             bits4=MathTex("1 \\oplus 1 = 0").scale(0.7).next_to(bits3,DOWN).set_color(BLACK)
 
+            # boxes around xor operations
+            boxbits=SurroundingRectangle(bits, buff=0.1).set_color(GOLD)
+            boxbits2=SurroundingRectangle(bits2, buff=0.1).set_color(GOLD)
+            boxbits3=SurroundingRectangle(bits3, buff=0.1).set_color(GOLD)
+            boxbits4=SurroundingRectangle(bits4, buff=0.1).set_color(GOLD)
+
+            # show that it is like a sum
+            sum1=MathTex("0 + 0 = 0").scale(0.7).next_to(bits,2*RIGHT).set_color(BLACK)
+            sum2=MathTex("0 + 1 = 1").scale(0.7).next_to(bits2,2*RIGHT).set_color(BLACK)
+            sum3=MathTex("1 + 0 = 1").scale(0.7).next_to(bits3,2*RIGHT).set_color(BLACK)
+            sum4=MathTex("1 + 1 = 2").scale(0.7).next_to(bits4,2*RIGHT).set_color(BLACK)
+
+            #boxes around the sum
+            box1=SurroundingRectangle(sum1, buff=0.1).set_color(GOLD)
+            box2=SurroundingRectangle(sum2, buff=0.1).set_color(GOLD)
+            box3=SurroundingRectangle(sum3, buff=0.1).set_color(GOLD)
+            box4=SurroundingRectangle(sum4, buff=0.1).set_color(GOLD)
+
             # xor operation equation general
             xor=MathTex("a \\oplus b = c").scale(0.7).shift(UP).set_color(BLACK)
 
             # xor operation equation with mod
             xor2=MathTex("(a+b)\\mod 2 \\equiv c ").scale(0.7).shift(UP).set_color(BLACK)
 
-            self.play(Write(texto1))
-            self.play(Write(bits),Write(bits2),Write(bits3),Write(bits4))
+            # xor operation showing 2 mod 2 = 0
+            xor3=MathTex("2\\mod 2 = 0").scale(0.7).shift(UP).set_color(BLACK)
+
+
+
+
+
+            # self.play(Write(texto1))
+            # self.play(Write(bits),Write(bits2),Write(bits3),Write(bits4))
+            # self.wait(1)
+            # self.play(Write(sum1),Write(sum2),Write(sum3),Write(sum4))
+            # self.wait(1)
+            # self.play(Write(boxbits),Write(boxbits2),Write(boxbits3),Write(boxbits4))
+            # self.wait(1)
+            # self.play(
+            #     ReplacementTransform(boxbits,box1),
+            #     ReplacementTransform(boxbits2,box2),
+            #     ReplacementTransform(boxbits3,box3),
+            #     ReplacementTransform(boxbits4,box4)
+            # )
+            # self.play(box4.animate.set_color(RED))
+
+            # self.wait(1)
+
 
 
 
@@ -170,5 +235,6 @@ class OTP(Scene):
 
             self.wait()
 
-        xor_is_reversible_animation()
+        intro()
+        #xor_is_reversible_animation()
         #message_send_animation()
