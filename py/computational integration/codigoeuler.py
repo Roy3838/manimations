e663347f3b33a560da5b6a5fe9fabbb27ad95188
Roy3838 <a01283783@itesm.com>
2023-01-23 19:49:22 -0600

    Euler animation

--- py/computational integration/codigoeuler.py
+++ py/computational integration/codigoeuler.py
@@ -4,11 +4,14 @@ class CodeFromString(Scene):
     def construct(self):
         code = '''import numpy as np
 
-y, x = np.zeros(100), np.zeros(100)
-lambda f(x) : -9.81*x
+xi, xf, dx, vi = 0, 1, 0.1, 5
 
-for i in range(tf,ti,dt):
-    y[i + 1] = y[i] + dt * f(x[i])
+x = np.linspace(xi, xf, int((xf - xi) / dx))
+y = np.zeros(len(x))
+v = lambda t: -9.81*t + vi
+
+for i in range(xi, len(x)-1, 1):
+    y[i + 1] = y[i] + dx * v(x[i])
 '''
         rendered_code = Code(code=code, tab_width=4, background="window",
                             language="Python", font="Monospace", line_spacing=0.8)
--- py/computational integration/placeholder.py
+++ py/computational integration/placeholder.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-ti, tf, dt = 0, 100, 1
-y, x = np.zeros(tf), np.zeros(tf)
-f = lambda t: -9.81*t + 5
-for i in range(ti,tf-1,dt):
-    y[i + 1] = y[i] + dt * f(x[i])
-    print(y[i])
+xi, xf, dx, vi = 0, 1, 0.1, 5
+
+x = np.linspace(xi, xf, int((xf - xi) / dx))
+y = np.zeros(len(x))
+v = lambda t: -9.81*t + vi
+
+for i in range(xi, len(x)-1, 1):
+    y[i + 1] = y[i] + dx * v(x[i])
+
