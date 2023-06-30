c8bfab2cf5323f54402d74657cf0c969f5b802ec
Roy3838 <a01283783@itesm.mx>
2023-06-30 14:00:26 -0600

    Reoganize Repo

--- py/interferometro/interferometro.py
+++ py/interferometro/interferometro.py
@@ -23,19 +23,14 @@ class TwoPointInterference(ThreeDScene):
             return radius.animate.set_value(radiusfinal)
 
         ''' PART 1 PLANO GENERAL'''
-
         
 
         #Create two points
         fuente1=Dot(color=BLUE).shift(3*LEFT)
         fuente2=Dot(color=BLUE).shift(3*RIGHT)
-
-        
             
-
         analysispoint=Dot(color=BLACK)
         
-
         distance1=Line(fuente1,analysispoint).set_color(GOLD)
         distance2=Line(fuente2,analysispoint).set_color(GOLD)
 
@@ -191,7 +186,6 @@ class TwoPointInterference(ThreeDScene):
         def twodistances():
             """ PART 2 DOS LINEAS POR SEPARADO"""
 
-    
 
             '''ANIMACIONES 2'''
             lambda0=0.785398163397
@@ -274,192 +268,7 @@ class TwoPointInterference(ThreeDScene):
             self.play(FadeOut(lambdadf3),FadeOut(deltadf3))
             self.wait()
             self.play(FadeOut(lambdad),FadeOut(deltad))
-
-
-        def young():
-            def Create_semi(fuente):
-                #Create semi wave
-                radius = ValueTracker(0.01)
-                radiusfinal=7
-                wave=Arc(radius=radius.get_value(),start_angle=-90*DEGREES,angle=PI,color=BLUE).move_to(fuente.get_center())
-                wave.add_updater(lambda m: m.become(
-                    Arc(radius=radius.get_value(),start_angle=-90*DEGREES,angle=PI,color=BLUE).align_to(fuente,direction=LEFT).set_opacity((1-(1/radiusfinal)*radius.get_value())).set_y(fuente.get_y())
-                    ))
-                
-                self.add(wave)
-                return radius.animate.set_value(radiusfinal)
-            
-            def Create_wave(fuente):
-                #Create wave with circle
-                radius = ValueTracker(0.01)
-                radiusfinal=5
-                wave=Circle(radius=radius.get_value(),color=BLUE).move_to(fuente.get_center())
-                wave.add_updater(lambda m: m.become(Circle(radius=radius.get_value(),color=BLUE).move_to(fuente.get_center()).set_opacity((1-(1/radiusfinal)*radius.get_value()))))
-                self.add(wave)
-                return radius.animate.set_value(radiusfinal)
-            #fuente de la izquierda
-            fuente1=Dot(color=BLUE).shift(6*LEFT)
-            #fuentes que representan las rendijas
-            fuente2=Dot(color=BLUE).shift(UP)
-            fuente3=Dot(color=BLUE).shift(DOWN)
-            #rejilla entre las dos fuentes
-            rejilla1=Rectangle(height=np.abs(fuente2.get_y()-fuente3.get_y()-0.2), width=0.2,color=BLACK).move_to(fuente2.get_x())
-            #rejillas arriba y abajo
-            rejilla2=Rectangle(height=4, width=0.2,color=BLACK).align_to(fuente2,direction=DOWN).shift(UP*0.2)
-            rejilla3=Rectangle(height=4, width=0.2,color=BLACK).align_to(fuente3,direction=UP).shift(DOWN*0.2)
-            #pantalla
-            pantalla=Rectangle(height=8,width=0.2,color=BLACK).move_to(RIGHT*6)
-            
-            #distancias
-            analysispoint=Dot(color=RED).move_to(pantalla.get_center())
-            distance1=Line(fuente2,analysispoint).set_color(GOLD)
-            distance2=Line(fuente3,analysispoint).set_color(GOLD)
-
-            distance1.add_updater(lambda x: x.become(
-                Line(fuente2,analysispoint).set_color(GOLD)
-            ))
-            distance2.add_updater(lambda x: x.become(
-                Line(fuente3,analysispoint).set_color(GOLD)
-            ))
-            
-            #animaciones
-            self.add(fuente1,rejilla1,rejilla2,rejilla3,pantalla)
-            self.play(Create_wave(fuente1),run_time=1.5)
-            self.play(Create_semi(fuente2),Create_semi(fuente3),run_time=1.5)
-            self.play(Create_wave(fuente1),run_time=1.5)
-            self.play(Create_semi(fuente2),Create_semi(fuente3),run_time=1.5)
-            self.play(FadeIn(fuente2),FadeIn(fuente3))
-            self.play(Create_wave(fuente2),Create_wave(fuente3),run_time=1.5)
-            self.play(Create_wave(fuente2),Create_wave(fuente3),run_time=1.5)
-            self.wait()
-            self.play(Create(distance1),Create(distance2),Create(analysispoint),run_time=1.5)
-            self.wait()
-            self.play(analysispoint.animate.shift(UP))
-            self.wait()
-            self.play(Create_wave(fuente2),Create_wave(fuente3),run_time=1.5)
-            self.play(analysispoint.animate.shift(DOWN*3),run_time=2)
-            self.play(Create_wave(fuente2),Create_wave(fuente3),run_time=1.5)
-            self.wait()
-
-            self.play(FadeOut(distance1),FadeOut(distance2),FadeOut(analysispoint))
-            self.play(FadeOut(fuente1),FadeOut(fuente2),FadeOut(fuente3),FadeOut(rejilla1),FadeOut(rejilla2),FadeOut(rejilla3),FadeOut(pantalla))
-            
-
-        def michelson():
-            def Create_wave(fuente):
-                #Create wave with circle
-                radius = ValueTracker(0.01)
-                radiusfinal=5
-                wave=Circle(radius=radius.get_value(),color=BLUE).move_to(fuente.get_center())
-                wave.add_updater(lambda m: m.become(Circle(radius=radius.get_value(),color=BLUE).move_to(fuente.get_center()).set_opacity((1-(1/radiusfinal)*radius.get_value()))))
-                self.add(wave)
-                return radius.animate.set_value(radiusfinal)
-            #recibe los parámetros de una onda y te regresa el Mobject
-            def onda(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2,override=0):
-                if override==0:
-                    override=pos2
-                elif override ==1:
-                    override=pos1
-                    
-                    
-                length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
-                sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
-                
-                sound2.add_updater(lambda x: x.become(
-                    ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
-                    .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
-                    .align_to(override, direction=RIGHT)
-                    ))
-                return sound2
-            def onda_vert(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2):
-                length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
-                sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
-                
-                sound2.add_updater(lambda x: x.become(
-                    ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
-                    .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
-                    .move_to([(pos1[0]+pos2[0])/2,(pos1[1]+pos2[1] + start.get_value())/2,0])
-                    ))
-                return sound2
-            
-            def onda_vertabajo(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2):
-                length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
-                sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
-                
-                sound2.add_updater(lambda x: x.become(
-                    ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
-                    .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
-                    .move_to([(pos1[0]+pos2[0])/2,(pos1[1]+pos2[1] - start.get_value())/2,0])
-                    ))
-                return sound2
-
-            def onda_regreso(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2,override=0):
-                length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
-                sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
-                
-                sound2.add_updater(lambda x: x.become(
-                    ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
-                    .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
-                    .move_to([(pos1[0]+pos2[0] + start.get_value())/2,(pos1[1]+pos2[1])/2,0])
-                    ))
-                return sound2
-            
-
-            #fuente original que se convierte en laser
-            fuente=Dot(color=BLUE).shift(6*LEFT)
-            laser=Rectangle(height=0.2,width=1,color=BLUE).shift(LEFT*6)
-            #splitter de en medio
-            splitter=Rectangle(height=0.1,width=0.7,color=GREY).rotate(PI/4)
-            #los dos espejos, el a es el de arriba y el b el de abajo
-            espejoa=Rectangle(height=0.1,width=0.7,color=GREY_D).shift(3.5*UP)
-            espejob=Rectangle(height=0.1,width=0.7,color=GREY_D).shift(3.5*RIGHT).rotate(PI/2)
-            #pantalla de observación de abajo
-            pantalla=Rectangle(height=0.1,width=5,color=BLACK).move_to(DOWN*3.5)
-            #onda antes de splitter
-            onda1tracker=ValueTracker(0)
-            onda1=onda(laser.get_right(),splitter.get_left(),BLUE,onda1tracker)
-
-            #ondas despues de splitter (onda pa riba y luego pa derecha)
-            onda2tracker=ValueTracker(0)
-            onda2=onda_vert(espejoa.get_bottom(),splitter.get_top(),BLUE,onda2tracker)
-            onda3tracker=ValueTracker(0)
-            onda3=onda(splitter.get_right(),espejob.get_left(),BLUE,onda3tracker)
-
-            #ondas despues de espejo
-            onda4tracker=ValueTracker(0)
-            onda4=onda_vertabajo(splitter.get_top(),espejoa.get_bottom(),BLUE,onda4tracker,difface=2)
-            onda5tracker=ValueTracker(3.1)
-            #onda5segundotracker=ValueTracker(1) ,segundotracker=onda5segundotracker
-            onda5=onda_regreso(espejob.get_left(),splitter.get_right(),BLUE,onda5tracker,difface=2)
-
-            onda6tracker=ValueTracker(0)
-            onda6=onda_vertabajo(pantalla.get_top(),splitter.get_bottom(),BLUE,onda6tracker,difface=2)
-            onda7tracker=ValueTracker(0)
-            onda7=onda_vertabajo(pantalla.get_top(),splitter.get_bottom(),BLUE,onda7tracker,difface=0)
-
-            #láser
-            self.add(fuente,splitter,espejoa,espejob,pantalla)
-            self.play(Create_wave(fuente))
-            self.play(Create_wave(fuente))
-            self.play(ReplacementTransform(fuente,laser))
-            self.play(Create(onda1),run_time=1.5,rate_func=linear)
-            self.play(Create(onda2),Create(onda3),onda1tracker.animate.set_value(5.157),run_time=1.5,rate_func=linear)
-            self.remove(onda1)
-            self.add(onda5)
-            self.play(onda2tracker.animate.set_value(3.1),onda3tracker.animate.set_value(3.1)
-                    ,Create(onda4),onda5tracker.animate.set_value(0),run_time=1.5,rate_func=linear)
-            self.remove(onda2,onda3,onda5)
-
-
-            self.play(onda4tracker.animate.set_value(3.1), Uncreate(onda5),Create(onda6),Create(onda7), run_time=1.5,rate_func=linear)
-            self.remove(onda4)
-
-            self.play(onda6tracker.animate.set_value(3.1),onda7tracker.animate.set_value(3.1),run_time=1.5,rate_func=linear)
-            self.remove(onda6,onda7)
-
-            self.wait()
-            self.play(FadeOut(laser),FadeOut(splitter),FadeOut(espejoa),FadeOut(espejob),FadeOut(pantalla))
-            
+      
             
         def hyperbolic():
             #modo es el lambda y d es el valuetracker de la distancia
@@ -533,7 +342,5 @@ class TwoPointInterference(ThreeDScene):
         twopoints()
         twodistances()
         #mathequations()
-        young()
-        michelson()
         hyperbolic()
 
---
+++ py/interferometro/michelson.py
@@ -0,0 +1,122 @@
+
+
+from manim import *
+import numpy as np
+
+class Michelson(MovingCameraScene):
+    def construct(self):
+
+        def Create_wave(fuente):
+            #Create wave with circle
+            radius = ValueTracker(0.01)
+            radiusfinal=5
+            wave=Circle(radius=radius.get_value(),color=BLUE).move_to(fuente.get_center())
+            wave.add_updater(lambda m: m.become(Circle(radius=radius.get_value(),color=BLUE).move_to(fuente.get_center()).set_opacity((1-(1/radiusfinal)*radius.get_value()))))
+            self.add(wave)
+            return radius.animate.set_value(radiusfinal)
+        #recibe los parámetros de una onda y te regresa el Mobject
+        def onda(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2,override=0):
+            if override==0:
+                override=pos2
+            elif override ==1:
+                override=pos1
+                
+                
+            length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
+            sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
+            
+            sound2.add_updater(lambda x: x.become(
+                ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
+                .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
+                .align_to(override, direction=RIGHT)
+                ))
+            return sound2
+        def onda_vert(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2):
+            length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
+            sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
+            
+            sound2.add_updater(lambda x: x.become(
+                ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
+                .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
+                .move_to([(pos1[0]+pos2[0])/2,(pos1[1]+pos2[1] + start.get_value())/2,0])
+                ))
+            return sound2
+        
+        def onda_vertabajo(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2):
+            length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
+            sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
+            
+            sound2.add_updater(lambda x: x.become(
+                ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
+                .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
+                .move_to([(pos1[0]+pos2[0])/2,(pos1[1]+pos2[1] - start.get_value())/2,0])
+                ))
+            return sound2
+
+        def onda_regreso(pos1,pos2,color,start=ValueTracker(0),difface=0,periodo=8,amplitud=0.2,override=0):
+            length=np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
+            sound2=ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([0, length]), fill_opacity=0).set_color(color)
+            
+            sound2.add_updater(lambda x: x.become(
+                ParametricFunction(lambda t: np.array((t, amplitud*np.sin(periodo * (t + difface)), 0)), t_range = np.array([start.get_value(), length]), fill_opacity=0).set_color(color)
+                .rotate(np.arctan((pos1[1]-pos2[1])/(pos1[0]-pos2[0])),about_point=pos1)
+                .move_to([(pos1[0]+pos2[0] + start.get_value())/2,(pos1[1]+pos2[1])/2,0])
+                ))
+            return sound2
+        
+
+        #fuente original que se convierte en laser
+        fuente=Dot(color=BLUE).shift(6*LEFT)
+        laser=Rectangle(height=0.2,width=1,color=BLUE).shift(LEFT*6)
+        #splitter de en medio
+        splitter=Rectangle(height=0.1,width=0.7,color=GREY).rotate(PI/4)
+        #los dos espejos, el a es el de arriba y el b el de abajo
+        espejoa=Rectangle(height=0.1,width=0.7,color=GREY_D).shift(3.5*UP)
+        espejob=Rectangle(height=0.1,width=0.7,color=GREY_D).shift(3.5*RIGHT).rotate(PI/2)
+        #pantalla de observación de abajo
+        pantalla=Rectangle(height=0.1,width=5,color=BLACK).move_to(DOWN*3.5)
+        #onda antes de splitter
+        onda1tracker=ValueTracker(0)
+        onda1=onda(laser.get_right(),splitter.get_left(),BLUE,onda1tracker)
+
+        #ondas despues de splitter (onda pa riba y luego pa derecha)
+        onda2tracker=ValueTracker(0)
+        onda2=onda_vert(espejoa.get_bottom(),splitter.get_top(),BLUE,onda2tracker)
+        onda3tracker=ValueTracker(0)
+        onda3=onda(splitter.get_right(),espejob.get_left(),BLUE,onda3tracker)
+
+        #ondas despues de espejo
+        onda4tracker=ValueTracker(0)
+        onda4=onda_vertabajo(splitter.get_top(),espejoa.get_bottom(),BLUE,onda4tracker,difface=2)
+        onda5tracker=ValueTracker(3.1)
+        #onda5segundotracker=ValueTracker(1) ,segundotracker=onda5segundotracker
+        onda5=onda_regreso(espejob.get_left(),splitter.get_right(),BLUE,onda5tracker,difface=2)
+
+        onda6tracker=ValueTracker(0)
+        onda6=onda_vertabajo(pantalla.get_top(),splitter.get_bottom(),BLUE,onda6tracker,difface=2)
+        onda7tracker=ValueTracker(0)
+        onda7=onda_vertabajo(pantalla.get_top(),splitter.get_bottom(),BLUE,onda7tracker,difface=0)
+
+        #láser
+        self.add(fuente,splitter,espejoa,espejob,pantalla)
+        self.play(Create_wave(fuente))
+        self.play(Create_wave(fuente))
+        self.play(ReplacementTransform(fuente,laser))
+        self.play(Create(onda1),run_time=1.5,rate_func=linear)
+        self.play(Create(onda2),Create(onda3),onda1tracker.animate.set_value(5.157),run_time=1.5,rate_func=linear)
+        self.remove(onda1)
+        self.add(onda5)
+        self.play(onda2tracker.animate.set_value(3.1),onda3tracker.animate.set_value(3.1)
+                ,Create(onda4),onda5tracker.animate.set_value(0),run_time=1.5,rate_func=linear)
+        self.remove(onda2,onda3,onda5)
+
+
+        self.play(onda4tracker.animate.set_value(3.1), Uncreate(onda5),Create(onda6),Create(onda7), run_time=1.5,rate_func=linear)
+        self.remove(onda4)
+
+        self.play(onda6tracker.animate.set_value(3.1),onda7tracker.animate.set_value(3.1),run_time=1.5,rate_func=linear)
+        self.remove(onda6,onda7)
+
+        self.wait()
+        self.play(FadeOut(laser),FadeOut(splitter),FadeOut(espejoa),FadeOut(espejob),FadeOut(pantalla))
+        
