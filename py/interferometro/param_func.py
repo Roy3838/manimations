3dd0d24adb16b42add1554dd0da9e777caf81bc8
Roy3838 <a01283783@itesm.mx>
2023-08-09 15:15:06 -0600

    waves added to param animation

--- py/interferometro/param_func.py
+++ py/interferometro/param_func.py
@@ -7,6 +7,9 @@ class ParametricSurface(ThreeDScene):
     def construct(self):
         self.camera.background_color = "#E2E2E2"
 
+
+        """ BEGINING OF DECLARING FUNCTIONS AND UPDATERS """
+
         def wave_updater(mobject, dt):
             #Create wave
             mobject.radius += 0.01 + 3*dt
@@ -19,12 +22,11 @@ class ParametricSurface(ThreeDScene):
         def begin_flashing(mobject, dt):
             mobject.time += dt
             if mobject.time > 0.5 and mobject.flashing == True:
-                flash = Circle(radius=0.01).move_to(mobject.get_center())
+                flash = Circle(radius=0.01).move_to(mobject.get_center()).set_color(BLUE)
                 flash.radius = 0.01
                 flash.add_updater(wave_updater)
                 self.add(flash)
                 mobject.time = 0   
-
 
         def func(t, theta, h=0, a=1, k=0, b=3):
             x = h + a / np.cos(t)
@@ -32,18 +34,58 @@ class ParametricSurface(ThreeDScene):
             z = (k + b * np.tan(t)) * np.sin(theta)
             return np.array([x, y, z])
         
+        def wave(mobject, dt):
+
+            def wave_function(t):
+                return np.array((t,
+                                 (1/1.54)*np.arctan(4*t)*A*np.sin(P * t - wave1.t_offset),
+                                 0))
+            
+            mobject.t_offset += dt * mobject.phi
+
+            A = mobject.amplitud
+            P = mobject.periodo
+
+            origin_coordinate = mobject.origin.get_center()
+            target_coordinate = mobject.im_target.get_center()
 
 
-        # DEFINICION OF THE TWO SOURCES
-        fuente1=Dot(color=BLUE).shift(5*LEFT)
-        fuente2=Dot(color=BLUE).shift(5*RIGHT)
-        fuente1.time = 0
-        fuente1.flashing = False
-        fuente1.add_updater(begin_flashing)
-        fuente2.time = 0
-        fuente2.flashing = False
-        fuente2.add_updater(begin_flashing)
+            # Norm of the difference of the two points
 
+            cartesian_distance = np.linalg.norm(target_coordinate - origin_coordinate)
+
+            range = np.array([0, cartesian_distance])   
+
+            # Get angle of rotation
+            angle = np.arctan2(target_coordinate[1] - origin_coordinate[1],
+                                 target_coordinate[0] - origin_coordinate[0])
+
+
+            mobject.become(
+                ParametricFunction(
+                    wave_function,
+                    t_range=range,
+                    fill_opacity = 0)
+                    .set_color(mobject.get_color())
+                    .align_to(mobject.origin,LEFT)
+                    .shift(RIGHT*0.1)
+                    .set_y(mobject.origin.get_y())
+                    .rotate(angle, about_point = mobject.origin.get_center())
+            )
+
+        def init_wave(from_origin, to_target):
+            wave = ParametricFunction(lambda t: np.array((t,t,0))).set_color(BLACK)
+            wave.amplitud = 0.8
+            wave.periodo = 4
+            wave.phi = 6
+            wave.t_offset = 0
+            wave.origin = Dot(color = BLACK).move_to(from_origin)
+            wave.target = Dot(color = BLUE).move_to(to_target)
+            wave.im_target = Dot(color = BLUE).move_to(to_target)
+            return wave
+
+
+        """ BEGINING OF DECLARING MOBJECTS """
 
         axes = ThreeDAxes(
             x_range=[-4,4],
@@ -57,6 +99,26 @@ class ParametricSurface(ThreeDScene):
             z_axis_config={"include_tip": False},
             ).set_color(BLACK)
 
+
+        # DEFINICION OF THE TWO SOURCES
+        fuente1=Dot(color=BLUE).shift(5*LEFT)
+        fuente2=Dot(color=BLUE).shift(5*RIGHT)
+        fuente1.time = 0
+        fuente1.flashing = False
+        fuente1.add_updater(begin_flashing)
+        fuente2.time = 0
+        fuente2.flashing = False
+        fuente2.add_updater(begin_flashing)
+
+
+        # DEFINICION OF THE WAVES
+        wave1 = init_wave(fuente1.get_center(), ORIGIN)        
+        wave1.add_updater(wave)
+        wave2 = init_wave(fuente2.get_center(), ORIGIN)
+        wave2.add_updater(wave)
+
+
+        
 
         surfaces = VGroup()
         params = VGroup()
@@ -80,13 +142,46 @@ class ParametricSurface(ThreeDScene):
             surfaces.add(surface)
             params.add(param)
 
+
+
+
+        self.add(axes)
+
+
         self.play(Create(fuente1),Create(fuente2))
         fuente1.flashing = True
         fuente2.flashing = True
 
+
         self.play(Create(params),Create(axes.x_axis),Create(axes.y_axis))
 
         self.wait()
+
+        fuente1.flashing = False
+        fuente2.flashing = False
+
+        self.wait()
+
+        self.play(Create(wave1),
+                  Create(wave1.origin),
+                  Create(wave1.im_target),
+                  Create(wave2),
+                  Create(wave2.origin),
+                  Create(wave2.im_target),)
+
+        self.play(wave1.im_target.animate.move_to(params[1].get_bottom()),
+                  wave2.im_target.animate.move_to(params[1].get_bottom()),
+        )
+
+        self.play(MoveAlongPath(wave1.im_target, params[1]),
+                  MoveAlongPath(wave2.im_target, params[1]),
+                   run_time = 5)
+
+        self.play(wave1.im_target.animate.move_to(ORIGIN),
+                  wave2.im_target.animate.move_to(ORIGIN),
+        )
+
+        self.wait(3)
                
 
 
@@ -94,23 +189,23 @@ class ParametricSurface(ThreeDScene):
 
         # MAKE IT 3D
 
-        self.move_camera(phi=10 * DEGREES, theta=30 * DEGREES, gamma = 115 * DEGREES)
+        # self.move_camera(phi=10 * DEGREES, theta=30 * DEGREES, gamma = 115 * DEGREES)
 
-        self.play(Create(axes.z_axis))
+        # self.play(Create(axes.z_axis))
 
-        self.play(Rotate(axes, 2*PI, axis=RIGHT, about_point=ORIGIN),
-                  Rotate(params, 2*PI, axis=RIGHT, about_point=ORIGIN),
-                   run_time=2)
+        # self.play(Rotate(axes, 2*PI, axis=RIGHT, about_point=ORIGIN),
+        #           Rotate(params, 2*PI, axis=RIGHT, about_point=ORIGIN),
+        #            run_time=2)
         
-        self.wait()
+        # self.wait()
         
-        fuente1.flashing = False
-        fuente2.flashing = False 
+        # fuente1.flashing = False
+        # fuente2.flashing = False 
 
-        self.move_camera(phi=0 * DEGREES, theta=0 * DEGREES, gamma = 90 * DEGREES)
+        # self.move_camera(phi=0 * DEGREES, theta=0 * DEGREES, gamma = 90 * DEGREES)
 
-        self.play(Create(surfaces))
+        # self.play(Create(surfaces))
 
-        self.begin_3dillusion_camera_rotation(rate=2)
-        self.wait(3)
-        self.stop_3dillusion_camera_rotation()
+        # self.begin_3dillusion_camera_rotation(rate=2)
+        # self.wait(3)
+        # self.stop_3dillusion_camera_rotation()
--- py/interferometro/planewave.py
+++ py/interferometro/planewave.py
@@ -12,39 +12,41 @@ class TwoWavesDistance(Scene):
         def wave_updater(mobject, dt):
 
             def wave_function(t):
-                x, y = t
-                return np.array((x,
-                                y + (1/1.54)*np.arctan(4*x)*A*np.sin(P * x - mobject.t_offset),
-                                0))
-                    
+                return np.array((t,
+                                 (1/1.54)*np.arctan(4*t)*A*np.sin(P * t - wave1.t_offset),
+                                 0))
+            
             mobject.t_offset += dt * mobject.phi
+
             A = mobject.amplitud
             P = mobject.periodo
-            range_x = np.array([ np.abs(mobject.origin.get_x() - mobject.target.get_x())])
-            range_y = np.array([mobject.origin.get_y()- mobject.target.get_y()])
-            t_range = np.array()
+
+            origin_coordinate = mobject.origin.get_center()
+            target_coordinate = mobject.im_target.get_center()
+
+
+            # Norm of the difference of the two points
+
+            cartesian_distance = np.linalg.norm(target_coordinate - origin_coordinate)
+
+            range = np.array([0, cartesian_distance])   
+
+            # Get angle of rotation
+            angle = np.arctan2(target_coordinate[1] - origin_coordinate[1],
+                                 target_coordinate[0] - origin_coordinate[0])
+
 
             mobject.become(
                 ParametricFunction(
                     wave_function,
-                    t_range=t_range,
-                    fill_opacity=0)
+                    t_range=range,
+                    fill_opacity = 0)
                     .set_color(mobject.get_color())
-                    .align_to(mobject.target, RIGHT)
-                    .shift(LEFT*0.1)
+                    .align_to(mobject.origin,LEFT)
+                    .shift(RIGHT*0.1)
+                    .set_y(mobject.origin.get_y())
+                    .rotate(angle, about_point = mobject.origin.get_center())
                 )
-
-            # Move target point Dot()
-            x, y = range_x[1], range_y[1]
-            target_y = wave_function([x, y])[1]
-            mobject.target.move_to([mobject.target.get_x(), target_y, 0])
-
-            # Move superposition Arrow()
-            arrow_base = [mobject.target.get_x(), mobject.origin.get_y(), 0]
-            mobject.arrow.become(
-                Arrow(arrow_base, mobject.target.get_center(), buff=0)
-                .set_color(BLACK)
-                .shift(RIGHT))
 
 
         def init_wave():
@@ -55,15 +57,17 @@ class TwoWavesDistance(Scene):
             wave.t_offset = 0
             wave.origin = Dot(color = BLACK).move_to(LEFT*2)
             wave.target = Dot(color = BLUE).move_to(RIGHT*3)
-            wave.arrow = Arrow()
+            wave.im_target = Dot(color = BLUE).move_to(RIGHT*3)
             return wave
 
         wave1 = init_wave()
 
-        wave1.arrow = Arrow(wave1.origin.get_center(), wave1.target.get_center(), buff=0, color=BLACK)
-
-        self.add(wave1.origin, wave1.target, wave1.arrow, wave1)
+        
         wave1.add_updater(wave_updater)
+        self.add(wave1.origin, wave1.target, wave1.im_target, wave1)
+
+        self.play(wave1.origin.animate.shift(UP))
+        self.play(wave1.im_target.animate.shift(DOWN))
 
         self.wait(4)
 
