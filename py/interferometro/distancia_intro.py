e0d3347dd2b2653e3a8450df2f5eabe65c8c7920
Roy3838 <a01283783@itesm.mx>
2023-08-13 14:57:57 -0600

    update

---
+++ py/interferometro/distancia_intro.py
@@ -0,0 +1,9 @@
+from manim import *
+
+
+
+
+class Interferometer_Pythagoras(Scene):
+    def construct(self):
+        """ Scene that describes how the distance between points is calculated using pythagoras
+        of the coordinates of the points. """
