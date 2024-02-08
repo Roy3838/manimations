d6111d9b3233bccb44fc4e3d5f9d25662ef012a0
Roy3838 <A01283783@itesm.mx>
2024-02-08 12:07:38 -0600

    Spring mobject oscilator

--- py/harmonic/oscilator.py
+++ py/harmonic/oscilator.py
@@ -13,34 +13,67 @@ def terminalplot(x,y=None):
     fig.plot(x,y)
     fig.show()
 
+class Spring(VMobject):
+    def __init__(self, start=LEFT, end=RIGHT, num_coils=5, width=2, **kwargs):
+        super().__init__(**kwargs)
+        self.start = start
+        self.end = end
+        self.num_coils = num_coils
+        self.width = width
+        self.draw_spring()
+
+    def draw_spring(self):
+        self.clear_points()
+        direction = self.end - self.start
+        length = np.linalg.norm(direction)
+        direction = direction / length  # Normalize
+
+        # Calculate points for the spring
+        points = [self.start]
+        for i in range(1, self.num_coils + 1):
+            point = self.start + direction * (length / self.num_coils) * i
+            if i % 2 == 0:
+                points.append(point)
+            else:
+                offset = np.array([-direction[1], direction[0], 0]) * self.width
+                points.append(point + offset)
+
+        # Draw the spring
+        self.set_points_as_corners(points)
+
+    def put_start_and_end_on(self, start, end):
+        self.start = start
+        self.end = end
+        self.draw_spring()
+
+
 class OscillatingSquare(VGroup):
-    def __init__(self, phi, w=np.pi, opacity = 0.1, A=2, scale =0.5 , **kwargs):
+    def __init__(self, phi, w=np.pi, opacity=0.1, A=2, scale=0.5, **kwargs):
         super().__init__(**kwargs)
         self.square = Square(color=BLACK, fill_opacity=opacity).set_fill(GREY_C).scale(scale)
-        self.add(self.square)
+        self.spring = Spring().set_color(GREY).stretch_to_fit_height(1)
+        self.spring.add_updater(lambda m: m.put_start_and_end_on([0, 0, 0], self.square.get_center()))
+        self.add(self.spring, self.square)
+
         self.t = 0
         self.phi = phi
         self.w = w
         self.A = A
-        self.add_updater(self.oscilator_update)
+        self.add_updater(self.oscillator_update)
 
-    def oscilator_update(self, mobject, dt):
+    def oscillator_update(self, mobject, dt):
         self.t += dt
         self.square.move_to([self.A * np.sin(self.t * self.w + self.phi), 1, 0])
 
-class Oscilator(Scene):
+class Oscillator(Scene):
     def construct(self):
         self.camera.background_color = "#E2E2E2"
 
-        num_squares = 50  # Number of squares
+        num_squares = 1  # Number of squares
         phis = np.random.normal(0, 0.5, num_squares)  # Normally distributed phi values
         for phi in phis:
-            square = OscillatingSquare(phi, scale =sigmoid(0.3*np.abs(1/phi))-0.4)
+            square = OscillatingSquare(phi, scale=sigmoid(0.3*np.abs(1/phi))-0.4)
             self.add(square)
 
-
-        #plot in order from low to high
-        terminalplot(np.sort(phis))
         self.wait(10)
-
             
